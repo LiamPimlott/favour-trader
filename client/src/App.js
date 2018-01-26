4cf3b07e14cda9e14bfe2ec081209c4b09c58581
Liam Pimlott <liam.tj.pimlott@gmail.com>
2018-01-26 11:47:40 -0600

    make a simple fetch from react

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {welcomeMessage: []}
+  
+  componentDidMount() {
+    fetch('/')
+      .then(res => res.json())
+      .then(welcomeMessage => this.setState({ welcomeMessage }));
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to FavourTrader</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.welcomeMessage}
         </p>
       </div>
     );
