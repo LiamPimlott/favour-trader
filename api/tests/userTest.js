0012180d02fa13672d99d66fd8fdcb4993526317
ismail khowaja <ismailkhowaja1@gmail.com>
2018-02-02 00:54:02 -0500

    User model for mvp

---
+++ api/tests/userTest.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+var User = require('../models/users');
+
+var user = new User();
+user.save((error) => {
+	assert.equal(error.errors['name'].message, 'Name is required.');
+	assert.equal(error.errors['email'].message, 'Email Address is required.');
+	assert.equal(error.errors['password'].message, 'Password is required.');
+	assert.equal(error.errors['address'].message, 'Address is required.');	
+})
+
+var user1 = new User({name: {first: 'test', last: 'te'}});
+user1.address = {number: 20};
+user1.password = '123';
+user1.email = 'example@gmail.com';
+user1.save((error) => {
+	assert.equal(error.message, 'Last Name is too Short.');
+})
--- favour-trader/models/users.js
+++ favour-trader/models/users.js
@@ -1,7 +1,63 @@
 var mongoose = require("mongoose");
 
+var nameSchema = new mongoose.Schema({
+  first: {
+  	type:String
+  },
+  last: {
+  	type: String
+  }
+});
+
+nameSchema.pre('save', function (next) {
+	if (2 > this.first.length) {
+    	return next(new Error('First Name is too Short.'));
+  	}
+  	if (2 > this.last.length) {
+  		return next(new Error('Last Name is too Short.'));
+  	} 
+  next();
+})
+
+var addressSchema = new mongoose.Schema({
+	number: Number,
+	street: String,
+	postalCode: String,
+	city: String,
+	state: String,
+	country: String,
+})
+
+var objectId = mongoose.Schema.Types.ObjectId;
+var skillSchema = new mongoose.Schema({ skill: objectId });
+
 var user = new mongoose.Schema({
-   name: String,
+   name: {
+   	type: nameSchema,
+   	required: [true, 'Name is required.'],
+   },
+   email: {
+   	type: String,
+   	unique: true,
+  	required: [true, 'Email Address is required.']
+   },
+   password: {
+   	type: String,
+   	required: [true, 'Password is required.']
+   },
+   address: {
+	type: addressSchema,
+   	required: [true, 'Address is required.']
+   },
+   about: {
+   	type: String
+   },
+   has: {
+   	type: skillSchema
+   },
+   wants: {
+   	type: skillSchema
+   }
 });
 
 module.exports = mongoose.model("User", user);
