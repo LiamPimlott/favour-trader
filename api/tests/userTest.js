a6e46c656a55d612742618d825602b4783126c35
ismail khowaja <ismailkhowaja1@gmail.com>
2018-02-02 12:09:49 -0500

    indentation, semicolons and comments fixed

--- api/tests/userTest.js
+++ api/tests/userTest.js
@@ -1,18 +1,20 @@
 var assert = require('assert');
 var User = require('../models/users');
 
+// Create user with empty fields
 var user = new User();
 user.save((error) => {
 	assert.equal(error.errors['name'].message, 'Name is required.');
 	assert.equal(error.errors['email'].message, 'Email Address is required.');
 	assert.equal(error.errors['password'].message, 'Password is required.');
 	assert.equal(error.errors['address'].message, 'Address is required.');	
-})
+});
 
+//create the user with invalid last name
 var user1 = new User({name: {first: 'test', last: 'te'}});
 user1.address = {number: 20};
 user1.password = '123';
 user1.email = 'example@gmail.com';
 user1.save((error) => {
 	assert.equal(error.message, 'Last Name is too Short.');
-})
+});
--- favour-trader/models/users.js
+++ favour-trader/models/users.js
@@ -11,53 +11,53 @@ var nameSchema = new mongoose.Schema({
 
 nameSchema.pre('save', function (next) {
 	if (2 > this.first.length) {
-    	return next(new Error('First Name is too Short.'));
-  	}
-  	if (2 > this.last.length) {
-  		return next(new Error('Last Name is too Short.'));
-  	} 
+    return next(new Error('First Name is too Short.'));
+  }
+	if (2 > this.last.length) {
+    return next(new Error('Last Name is too Short.'));
+  }
   next();
-})
+});
 
 var addressSchema = new mongoose.Schema({
-	number: Number,
-	street: String,
-	postalCode: String,
-	city: String,
-	state: String,
-	country: String,
-})
+  number: Number,
+  street: String,
+  postalCode: String,
+  city: String,
+  state: String,
+  country: String,
+});
 
 var objectId = mongoose.Schema.Types.ObjectId;
 var skillSchema = new mongoose.Schema({ skill: objectId });
 
 var user = new mongoose.Schema({
-   name: {
-   	type: nameSchema,
-   	required: [true, 'Name is required.'],
-   },
-   email: {
+  name: {
+    type: nameSchema,
+    required: [true, 'Name is required.'],
+  },
+  email: {
    	type: String,
    	unique: true,
   	required: [true, 'Email Address is required.']
-   },
-   password: {
+  },
+  password: {
    	type: String,
    	required: [true, 'Password is required.']
    },
-   address: {
-	type: addressSchema,
+  address: {
+    type: addressSchema,
    	required: [true, 'Address is required.']
-   },
-   about: {
-   	type: String
-   },
-   has: {
-   	type: skillSchema
-   },
-   wants: {
-   	type: skillSchema
-   }
+  },
+  about: {
+    type: String
+  },
+  has: {
+    type: skillSchema
+  },
+  wants: {
+    type: skillSchema
+  }
 });
 
 module.exports = mongoose.model("User", user);
