0a472d7c41a0418c9b994189a1d0687f418e40f1
DyckEr <35355107+DyckEr@users.noreply.github.com>
2018-02-06 21:11:09 -0600

    Edits to form
    changed some types of forms to type="text" instead of nothing. Thought it would be more clear.

--- favour-trader/client/src/pages/CreateAccount.js
+++ favour-trader/client/src/pages/CreateAccount.js
@@ -9,47 +9,47 @@ class CreateAccount extends Component {
 	   
 		<FormGroup>
           <Label for="firstName">First Name </Label>
-          <Input name="firstName" id="firstName" placeholder=" ex. Joe " />
+          <Input type="text" name="firstName" id="firstName" placeholder=" ex. Joe " />
         </FormGroup>
 		<FormGroup>
           <Label for="lastName">Last Name </Label>
-          <Input name="password" id="lastName" placeholder=" ex. Blow " />
+          <Input type="text" name="password" id="lastName" placeholder=" ex. Blow " />
         </FormGroup>
 		<FormGroup>
           <Label for="streetNumber">Street Number </Label>
-          <Input name="password" id="lastName" placeholder=" ex. 100 " />
+          <Input type="text" name="streetNumber" id="streetNumber" placeholder=" ex. 100 " />
         </FormGroup>
 		<FormGroup>
           <Label for="street">Street </Label>
-          <Input name="password" id="lastName" placeholder=" ex. Favour Street " />
+          <Input type="text" name="street" id="street" placeholder=" ex. Favour Street " />
         </FormGroup>
 		<FormGroup>
           <Label for="postalCode">Postal Code/ZIP </Label>
-          <Input name="password" id="lastName" placeholder=" ex. A1A 1A1 " />
+          <Input type="text" name="postalCode" id="postalCode" placeholder=" ex. A1A 1A1 " />
         </FormGroup>
 		<FormGroup>
           <Label for="city">City </Label>
-          <Input name="password" id="lastName" placeholder=" ex. Winnipeg " />
+          <Input type="text" name="city" id="city" placeholder=" ex. Winnipeg " />
         </FormGroup>
 		<FormGroup>
           <Label for="state">State/Province </Label>
-          <Input name="password" id="lastName" placeholder=" ex. Manitoba " />
+          <Input type="text" name="state" id="state" placeholder=" ex. Manitoba " />
         </FormGroup>
         <FormGroup>
           <Label for="email">Email</Label>
-          <Input type="email" name="email" id="exampleEmail" placeholder=" ex. favourtrader@gmail.com" />
+          <Input type="email" name="email" id="email" placeholder=" ex. favourtrader@gmail.com" />
         </FormGroup>
 		   <FormGroup>
           <Label for="confirmEmail">Confirm Email </Label>
-          <Input type="email" name="email" id="exampleEmail"/>
+          <Input type="email" name="confirmEmail" id="confirmEmail"/>
         </FormGroup>
 		   <FormGroup>
           <Label for="password">Password </Label>
-          <Input type="password" name="email" id="exampleEmail" />
+          <Input type="password" name="password" id="password" />
         </FormGroup>
         <FormGroup>
           <Label for="confirmPassword">Confirm Password </Label>
-          <Input type="password" name="password" id="examplePassword"/>
+          <Input type="password" name="confirmPassword" id="confirmPassword"/>
         </FormGroup>
         <Button>Submit</Button>
 		
