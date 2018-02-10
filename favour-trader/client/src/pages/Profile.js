8f354780363e8f0f6bdb2834525b1ad86b2687f1
RedRect <chenmrkent@hotmail.com>
2018-02-11 03:27:15 +0800

    Update Profile.js

--- favour-trader/client/src/pages/Profile.js
+++ favour-trader/client/src/pages/Profile.js
@@ -11,45 +11,45 @@ class Profile extends Component {
     return (
       <div>      	
       	<Container fluid>
-      	<Jumbotron >
+      	  <Jumbotron >
       		<Row>
-      		<Col xs="6" sm="4">
+      		  <Col xs="6" sm="4">
       			<Media>
 			  <Media left href="#">
-			  <Media style={imgStyle} object src={userPic} alt="user's image" />
+			    <Media style={imgStyle} object src={userPic} alt="user's image" />
 			  </Media>
 			  <Media body>
-			  	<Media heading>
-			          user name
-			        </Media>
+			    <Media heading>
+			      user name
+			    </Media>
 			    his@email.com 
 			  </Media>
 		        </Media>
       			<Row noGutters><h6>user name</h6></Row>
-				<Row noGutters>An introduction is the first paragraph of a written research paper, or the first thing you say in an oral presentation, or the first thing people see, hear, or experience about your project. </Row>
-      		</Col>      		
-      		<Col >
+			<Row noGutters>An introduction is the first paragraph of a written research paper, or the first thing you say in an oral presentation, or the first thing people see, hear, or experience about your project. </Row>
+      		  </Col>      		
+      		  <Col >
       			<h3>Has:</h3>
       			<Row noGutters>
-      			<ListGroup>
-		        <ListGroupItem className="justify-content-between">Cras justo odio <Badge pill>14</Badge></ListGroupItem>
-		        <ListGroupItem className="justify-content-between">Dapibus ac facilisis in <Badge pill>2</Badge></ListGroupItem>
-		        <ListGroupItem className="justify-content-between">Morbi leo risus <Badge pill>1</Badge></ListGroupItem>
-		      	</ListGroup>
+      			  <ListGroup>
+				  <ListGroupItem className="justify-content-between">Cras justo odio <Badge pill>14</Badge></ListGroupItem>
+				  <ListGroupItem className="justify-content-between">Dapibus ac facilisis in <Badge pill>2</Badge></ListGroupItem>
+				  <ListGroupItem className="justify-content-between">Morbi leo risus <Badge pill>1</Badge></ListGroupItem>
+		      	  </ListGroup>
       			</Row>				
-      		</Col>
-      		<Col >
+      		  </Col>
+      		  <Col >
       		<h3>Wants:</h3>
       			<Row>
-      			<ListGroup>
-		        <ListGroupItem className="justify-content-between">Cras justo odio <Badge pill>14</Badge></ListGroupItem>
-		        <ListGroupItem className="justify-content-between">Dapibus ac facilisis in <Badge pill>2</Badge></ListGroupItem>
-		        <ListGroupItem className="justify-content-between">Morbi leo risus <Badge pill>1</Badge></ListGroupItem>
-		      	</ListGroup>
+      			  <ListGroup>
+		            <ListGroupItem className="justify-content-between">Cras justo odio <Badge pill>14</Badge></ListGroupItem>
+		            <ListGroupItem className="justify-content-between">Dapibus ac facilisis in <Badge pill>2</Badge></ListGroupItem>
+		            <ListGroupItem className="justify-content-between">Morbi leo risus <Badge pill>1</Badge></ListGroupItem>
+		      	  </ListGroup>
       			</Row>				
       		</Col>
-      		</Row>
-      	</Jumbotron>
+      	      </Row>
+      	  </Jumbotron>
       	</Container>
       </div>
     );
