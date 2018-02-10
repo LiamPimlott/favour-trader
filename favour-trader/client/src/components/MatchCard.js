4f9ad70d63f7fecaff45995ddf869e4b6a62059c
ismail khowaja <ismailkhowaja1@gmail.com>
2018-02-10 13:25:15 -0500

    card to show matched profiles

---
+++ favour-trader/client/src/components/MatchCard.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import {
+    Card, CardText, CardBody, Col,
+    CardTitle, CardSubtitle, Button
+} from 'reactstrap';
+
+class MatchCard extends Component {
+    render() {
+        const {user, reveal} = this.props;
+        return (
+            <Col sm="4" className={'pb-3'}>
+                <Card>
+                    <CardBody>
+                        <CardTitle>{user.name.first}</CardTitle>
+                        <CardSubtitle>{user.email}</CardSubtitle>
+                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's
+                            content.</CardText>
+                        <Button onClick={reveal}>Button</Button>
+                    </CardBody>
+                </Card>
+            </Col>
+        );
+    }
+}
+
+export default MatchCard;
--- favour-trader/client/src/pages/Main.js
+++ favour-trader/client/src/pages/Main.js
@@ -1,66 +1,72 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ContractModal from '../components/ContractModal.js';
 import axios from 'axios'
+import {Row} from 'reactstrap';
+import MatchCard from "../components/MatchCard";
 
 class Main extends Component {
-  constructor() {
-    super();
-    this.state = {
-      users: [],
-      selectedUser: {},
-      modalOpen: false,
-    };
+    constructor() {
+        super();
+        this.state = {
+            users: [],
+            selectedUser: {},
+            modalOpen: false,
+        };
 
-    this.toggleModal = this.toggleModal.bind(this);
-    this.renderUsers = this.renderUsers.bind(this);
-  }
+        this.toggleModal = this.toggleModal.bind(this);
+        this.renderUsers = this.renderUsers.bind(this);
+    }
 
-  componentDidMount() {
-    axios.get('/api/users/all')
-      .then(res => res.data)
-      .then(data => this.setState({ users: data }));
-  }
+    componentDidMount() {
+        axios.get('/api/users/all')
+            .then(res => res.data)
+            .then(data => this.setState({users: data}));
+    }
 
-  toggleModal(user) {
-    this.setState({
-      selectedUser: user,
-      modalOpen: !this.state.modalOpen,
-    });
-  }
+    toggleModal(user) {
+        this.setState({
+            selectedUser: user,
+            modalOpen: !this.state.modalOpen,
+        });
+    }
 
-  renderUsers(users) {
-    return (
-      <div>
-      {
-        (users) ? (
-          <ul>
-          {
-            users.map(function(user) {
-              return (<li key={user._id} onClick={this.toggleModal.bind(this, user)}> {user.email} </li>)
-            }, this)
-          }
-          </ul>
-        ) : (
-          ''
-        )
-      }
-      </div>
-    );
-  }
+    renderUsers(users) {
+        return (
+            <div>
+                {
+                    (users) ? (
+                        <div className={'container'}>
+                            <Row>
+                                {
+                                    users.map(function (user) {
+                                        return (<MatchCard key={user._id} user={user}
+                                                           reveal={this.toggleModal.bind(this, user)}/>)
+                                    }, this)
+                                }
+                            </Row>
+                        </div>
+                    ) : (
+                        ''
+                    )
+                }
+            </div>
+        );
+    }
 
-  render() {
-    return (
-      <div>
-        <header>
-          <h1>Welcome to FavourTrader</h1>
-        </header>
-        <div>
-          {this.renderUsers(this.state.users)}
-        </div>
-        <ContractModal isOpen={this.state.modalOpen} toggle={this.toggleModal.bind(this, {})} user={this.state.selectedUser}/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <header>
+                    <h1>Welcome to FavourTrader</h1>
+                </header>
+                <div>
+                    {this.renderUsers(this.state.users)}
+                </div>
+                <ContractModal isOpen={this.state.modalOpen} toggle={this.toggleModal.bind(this, {})}
+                               user={this.state.selectedUser}/>
+            </div>
+        );
+    }
 }
 
 export default Main;
