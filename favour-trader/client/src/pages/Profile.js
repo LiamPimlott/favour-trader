0551aaed657daede90b188b8caadd642031bce3a
RedRect <chenmrkent@hotmail.com>
2018-02-11 03:22:52 +0800

    update

--- favour-trader/client/src/pages/Profile.js
+++ favour-trader/client/src/pages/Profile.js
@@ -7,9 +7,6 @@ var imgStyle = {
   maxWidth: "100px",
 };
 class Profile extends Component {
-
-
-
   render() {
     return (
       <div>      	
@@ -18,16 +15,16 @@ class Profile extends Component {
       		<Row>
       		<Col xs="6" sm="4">
       			<Media>
-					<Media left href="#">
-			        	<Media style={imgStyle} object src={userPic} alt="user's image" />
-			      	</Media>
-			      	<Media body>
-			        	<Media heading>
-			          	user name
+			  <Media left href="#">
+			  <Media style={imgStyle} object src={userPic} alt="user's image" />
+			  </Media>
+			  <Media body>
+			  	<Media heading>
+			          user name
 			        </Media>
-			        his@email.com 
-			      	</Media>
-		    	</Media>
+			    his@email.com 
+			  </Media>
+		        </Media>
       			<Row noGutters><h6>user name</h6></Row>
 				<Row noGutters>An introduction is the first paragraph of a written research paper, or the first thing you say in an oral presentation, or the first thing people see, hear, or experience about your project. </Row>
       		</Col>      		
