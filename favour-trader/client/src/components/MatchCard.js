f7dd03072e4ced75113a6d90d6c945db00e66b07
DyckEr <35355107+DyckEr@users.noreply.github.com>
2018-02-20 15:09:20 -0600

    Added profile pic and 'about' section
    currently just displays my Facebook profile picture

--- favour-trader/client/src/components/MatchCard.js
+++ favour-trader/client/src/components/MatchCard.js
@@ -1,13 +1,20 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Card, Button, Col } from 'antd';
+import Avatar from 'react-avatar';
 
 class MatchCard extends Component {
     render() {
         const {user, reveal} = this.props;
         return (
             <Col span={8}>
-                <Card title={user.name.first + " " + user.name.last.charAt(0) + "."} style={{ width: 300 }}>
-                    <Button size={'sm'} color={'primary'} onClick={reveal}>View Profile</Button>
+                <Card title={user.name.first + " " + user.name.last.charAt(0) + "."} style={{ width: 300, marginTop: '10px' }}>
+					<Avatar style={{marginBottom: '20px'}}facebookId="768867659" size="100" round={true} /> //change to src={user.profilePicUrl} instead of facebookId=...
+					<textarea 
+						readOnly
+						style={{height: '100px', width: '225px', fontSize: '13px', overflow: "hidden"}}>
+						{user.about}
+					</textarea>
+                    <Button size={'sm'} color={'primary'} onClick={reveal} style={{marginTop: '10px'}}>View Profile</Button>
                 </Card>
             </Col>
         );
