a35296f42faac0276956887e28606bc40f613e69
kerosenecity <alex.mike.mark@gmail.com>
2018-02-21 15:07:05 -0600

    Wrote addition registration tests.

--- favour-trader/test/userAPITest.js
+++ favour-trader/test/userAPITest.js
@@ -28,29 +28,55 @@ const users = [
             first: "Mark",
             last: "Ripptoe"
         },
-        address:{
+        address: {
             postalCode: "r3p1z2"
         },
         email: "test@test.ca",
         password: "assword"
     },
-    {//user[2] is one for the direct database insertions.
-        name: {
-            first: "Crash",
-            last: "Test"
-        },
-        address:{
-            postalCode: "r0x0x0"
-        },
-        email: "bumbling_protagonist@rom.com",
-        password: "somethingmemorablelikethenameofapet"
-    }
+    {//user[2] is for the http requests. It doesn't have all the required info.
+        password: "assword",
+        firstName: "Mark",
+        lastName: "Ripptoe",
+        postalCode: "r3p1z2"
+    },
+    {//user[3] is for the http requests. It doesn't have all the required info.
+        email: "test@test.ca",
+        firstName: "Mark",
+        lastName: "Ripptoe",
+        postalCode: "r3p1z2"
+    },
+    {//user[4] is for the http requests. It doesn't have all the required info.
+        email: "test@test.ca",
+        password: "assword",
+        postalCode: "r3p1z2"
+    },
+    {//user[5] is for the http requests. It doesn't have all the required info.
+        email: "test@test.ca",
+        password: "assword",
+        firstName: "Mark",
+        lastName: "Ripptoe",
+    },
+    {//user[6] is for the http requests. It has invalid name info,
+        email: "test@test.ca",
+        password: "assword",
+        firstName: "M",
+        lastName: "Ripptoe",
+        postalCode: "r3p1z2"
+    },
+    {//user[7] is for the http requests. It has invalid name info,
+        email: "test@test.ca",
+        password: "assword",
+        firstName: "Mark",
+        lastName: "R",
+        postalCode: "r3p1z2"
+    },
 ];
 
-describe("User API Tests", function(){
+describe("User API Tests", function () {
     beforeEach(function (done) {
         const setting = new Promise(function (resolve) {
-            require('dotenv').config({path: __dirname + '/../.env'});
+            require('dotenv').config({ path: __dirname + '/../.env' });
             process.env.NODE_ENV = 'test';
             server = require('../server');
             let db = require("../db");
@@ -86,13 +112,13 @@ describe("User API Tests", function(){
 
         it("it should return one user.", function (done) {
             const newUser = new User(users[1]);
-            const saveUser = new Promise( (resolve) => {
-                newUser.save(()=>{
+            const saveUser = new Promise((resolve) => {
+                newUser.save(() => {
                     resolve('created');
                 })
             });
 
-            saveUser.then(()=>{
+            saveUser.then(() => {
                 chai.request(url)
                     .get('/api/users/all')
                     .end((err, res) => {
@@ -116,10 +142,11 @@ describe("User API Tests", function(){
                     done();
                 })
         });
+
         it("it should not make dupe user", function (done) {
             const newUser = new User(users[1]);
-            const saveUser = new Promise( (resolve) => {
-                newUser.save(()=>{
+            const saveUser = new Promise((resolve) => {
+                newUser.save(() => {
                     resolve('created');
                 })
             });
@@ -136,41 +163,113 @@ describe("User API Tests", function(){
                     })
             });
         });
-     });
 
-    describe("/MATCH", function(){
+        it("it should not make a user without an email", function (done) {
+            chai.request(url)
+                .post('/api/users/register')
+                .send(users[2])
+                .end((err, res) => {
+                    // console.log(JSON.stringify(res.body))
+                    res.body.should.have.property("success").eql(false);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
 
-     });
+        it("it should not make a user without a password", function (done) {
+            chai.request(url)
+                .post('/api/users/register')
+                .send(users[3])
+                .end((err, res) => {
+                    // console.log(JSON.stringify(res.body))
+                    res.body.should.have.property("success").eql(false);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
 
-    describe("/AUTH", function(){
-         
+        it("it should not make a user without a name", function (done) {
+            chai.request(url)
+                .post('/api/users/register')
+                .send(users[4])
+                .end((err, res) => {
+                    console.log(JSON.stringify(res.body))
+                    res.body.should.have.property("success").eql(false);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+
+        it("it should not make a user without a valid first name", function (done) {
+            chai.request(url)
+                .post('/api/users/register')
+                .send(users[6])
+                .end((err, res) => {
+                    console.log(JSON.stringify(res.body))
+                    res.body.should.have.property("success").eql(false);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+
+        it("it should not make a user without a valid last name", function (done) {
+            chai.request(url)
+                .post('/api/users/register')
+                .send(users[7])
+                .end((err, res) => {
+                    console.log(JSON.stringify(res.body))
+                    res.body.should.have.property("success").eql(false);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
+
+        it("it should not make a user without a postal code", function (done) {
+            chai.request(url)
+                .post('/api/users/register')
+                .send(users[5])
+                .end((err, res) => {
+                    // console.log(JSON.stringify(res.body))
+                    res.body.should.have.property("success").eql(false);
+                    res.body.should.be.a('object');
+                    done();
+                });
+        });
     });
 
-    describe("/PROFILE", function(){
-         
+    describe("/MATCH", function () {
+
     });
 
-    describe("/ID/PROFILE", function(){
-         
+    describe("/AUTH", function () {
+
     });
 
-    describe("/HAS", function(){
-         
+    describe("/PROFILE", function () {
+        
     });
 
-    describe("/WANTS", function(){
-         
+    describe("/ID/PROFILE", function () {
+
     });
 
-    describe("/LOGIN", function(){
-         
+    describe("/HAS", function () {
+
     });
 
-    describe("/UPDATE", function(){
-         
+    describe("/WANTS", function () {
+
     });
 
-    describe("/DELETE", function(){
-         
+    describe("/LOGIN", function () {
+
+    });
+
+    describe("/UPDATE", function () {
+
+    });
+
+    describe("/DELETE", function () {
+
     });
 });
