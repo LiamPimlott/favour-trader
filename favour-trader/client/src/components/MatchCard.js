e5804553562af2dec81475180565c4e34197a287
DyckEr <35355107+DyckEr@users.noreply.github.com>
2018-02-23 16:24:11 -0600

    Added overflow prevention
    About sections no longer overflow past the view profile button.

--- favour-trader/client/src/components/MatchCard.js
+++ favour-trader/client/src/components/MatchCard.js
@@ -12,7 +12,7 @@ class MatchCard extends Component {
 					<Avatar style={{marginBottom: '20px'}} src="http://lorempixel.com/400/200/" size="125" round={true} />
 					<a class="active" style={ {position: 'absolute', top: '50%', color:'blue'} } onClick={reveal}>Review Skills</a>
 					<p
-						style={{height: '100px', width: '225px', fontSize: '13px'}}>
+						style={{overflow: 'hidden', marginTop: '25px', height: '120px', width: '225px', fontSize: '13px'}}>
 							{user.about}
 					</p>
                     <Button type="primary" style={{marginTop: '10px'}}>
