83e053cb6b099accb914aa2814ce80d1a54a912c
Liam Pimlott <liam.tj.pimlott@gmail.com>
2018-03-02 18:39:02 -0600

    Create SkillsList component

---
+++ favour-trader/client/src/components/SkillsList.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { List, Tabs, Button, Card } from 'antd';
+const TabPane = Tabs.TabPane;
+
+
+class SkillsList extends Component {
+    render() {
+        return (
+            <List
+                style={{overflow: 'scroll', height: '350px', padding: '0px 20px 0px 20px'}}
+                dataSource={this.props.skills}
+                renderItem={item => (
+                    <List.Item 
+                        key={item._id}
+                        actions={ this.props.isCurrentUser ? [<Button 
+                            type="danger"
+                            icon="delete"
+                            onClick={() => this.props.toggleDeleteSkillConfirm(this.props.skillSet, item._id)}
+                        />] : []}
+                        style={{padding: '20px'}}
+                    >
+                        <List.Item.Meta
+                            title={<a>{item.category.skill}</a>}
+                            description={item.description}
+                        />
+                    </List.Item>
+                )}
+            />
+            
+        )
+    }
+}
+
+export default SkillsList;
