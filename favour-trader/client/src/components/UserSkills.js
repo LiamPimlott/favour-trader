3f39e2f6175da55fb900d7a41640413047d05ce3
Liam Pimlott <liam.tj.pimlott@gmail.com>
2018-03-02 18:38:50 -0600

    Create UserSkills component

---
+++ favour-trader/client/src/components/UserSkills.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Button, Tabs, List, Card } from 'antd';
+import SkillsList from "./SkillsList";
+const TabPane = Tabs.TabPane;
+
+
+class UserSkills extends Component {
+    
+    render() {
+        return (
+            <Card bodyStyle={{padding: '0px'}}>
+                <Tabs 
+                    size="large" 
+                    tabBarStyle={{margin: '15px 0px 0px 0px'}}
+                    tabBarExtraContent={ this.props.isCurrentUser ? (<Button
+                        onClick={this.props.toggleNewSkillModal}
+                        type="primary"
+                        icon="plus" 
+                        style={{marginRight: "15px"}}
+                    >
+                        New Skill
+                    </Button>) : ''}
+                >
+                    <TabPane tab="Skills I Have" key="has">
+                        <SkillsList
+                            skillSet="has"
+                            skills={this.props.skills["has"]}
+                            isCurrentUser={this.props.isCurrentUser}
+                            toggleDeleteSkillConfirm={this.props.toggleDeleteSkillConfirm}
+                        />
+                    </TabPane>
+                    <TabPane tab="Skills I Need" key="wants">
+                        <SkillsList
+                            skillSet="wants"
+                            skills={this.props.skills["wants"]}
+                            isCurrentUser={this.props.isCurrentUser}
+                            toggleDeleteSkillConfirm={this.props.toggleDeleteSkillConfirm}
+                        />
+                    </TabPane>
+                </Tabs>
+            </Card>
+        )
+    }
+}
+
+export default UserSkills
