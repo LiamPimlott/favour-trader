c0302226e35779910420549f4a2e87b5bd1290e3
RedRect <chenmrkent@hotmail.com>
2018-03-08 13:48:10 +0800

    update
    The test for page Contract passed.

--- favour-trader/client/src/test/components/TradeOverview.test.js
+++ favour-trader/client/src/test/components/TradeOverview.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import TradeCard from '../../components/TradeCard.js';
+import TradeOverview from '../../components/TradeOverview.js';
 import { mount, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import moxios from "moxios";
 import { MemoryRouter } from 'react-router-dom'
+import renderer from 'react-test-renderer';
 
 beforeEach(function () {
     configure({ adapter: new Adapter() });
@@ -13,11 +14,10 @@ beforeEach(function () {
 it('renders correctly', () => {
     const offererName = 'testor';
     const offereeName = 'testee';
-    const tradeStatus = 'completed' ；
+    const tradeStatus = 'completed';
     const tradeMessage = 'perfect! amazing!';
-    mount(
-        <MemoryRouter>
-            <TradeCard offererName={offererName} offereeName={offereeName} tradeStatus={tradeStatus} tradeMessage={tradeMessage} />
-        </MemoryRouter>
-    )
+    const tree = renderer
+    .create(<TradeOverview offererName={offererName} offereeName={offereeName} tradeStatus={tradeStatus} tradeMessage={tradeMessage} />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
