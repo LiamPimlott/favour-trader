e07a531385a1b8a0cfcc10a59681d8ad59c7945c
DyckEr <35355107+DyckEr@users.noreply.github.com>
2018-03-09 15:18:32 -0600

    Match Web App Forms
    changed forms to match that of the web app / backend

--- favour-trader-native/src/SignUp.js
+++ favour-trader-native/src/SignUp.js
@@ -17,33 +17,7 @@ export default class SignUp extends React.Component {
 				<Text style={styles.intro} >We just need a bit of info to get you going..</Text>  
 				
 				<KeyboardAvoidingView behavior="padding" style={styles.container}>
-					<ScrollView style={ {top: '5%'} } contentContainerStyle={{justifyContent: 'center'}}> 
-					
-						<Text style={styles.sectionDetail} >Account Details</Text>					
-						<TextInput
-							placeholder="username"
-							style={styles.input}
-							ref={(input) => this.usernameInput = input}
-							onChangeText={(username) => this.setState({username})}
-							onSubmitEditing={() => this.passwordInput.focus()} 
-						/>
-						<TextInput
-							placeholder="password"
-							style={styles.input}
-							secureTextEntry
-							ref={(input) => this.passwordInput = input}
-							onChangeText={(password) => this.setState({password})}
-							onSubmitEditing={() => this.passwordConfirmInput.focus()} 
-						/>
-						<TextInput
-							placeholder="confirm password"
-							style={styles.inputSectionEnd}
-							secureTextEntry 
-							ref={(input) => this.passwordConfirmInput = input}
-							onChangeText={(passwordConfirm) => this.setState({passwordConfirm})}
-							onSubmitEditing={() => this.firstNameInput.focus()} 
-						/>
-						<Text style={styles.sectionDetail} >Personal</Text>	
+					<ScrollView style={ {top: '20%'} } contentContainerStyle={{justifyContent: 'center'}}> 
 						<TextInput
 							placeholder="First Name" 
 							style={styles.input}
@@ -58,37 +32,28 @@ export default class SignUp extends React.Component {
 							onChangeText={(lastName) => this.setState({lastName})}
 							onSubmitEditing={() => this.emailInput.focus()} 
 						/>
-						<Text style={styles.sectionDetail} >Email</Text>	
 						<TextInput
 							placeholder="e-mail address" 
 							style={styles.input} 
 							ref={(input) => this.emailInput = input}
 							onChangeText={(email) => this.setState({email})}							
-							onSubmitEditing={() => this.emailConfirmInput.focus()} 
+							onSubmitEditing={() => this.passwordInput.focus()} 
 						/>
 						<TextInput
-							placeholder="confirm e-mail address" 
+							placeholder="Password"
 							style={styles.input}
-							ref={(input) => this.emailConfirmInput = input}
-							onChangeText={(emailConfirm) => this.setState({emailConfirm})}
+							secureTextEntry
+							ref={(input) => this.passwordInput = input}
+							onChangeText={(password) => this.setState({password})}
 						/>
 						<TouchableOpacity 
 							style={styles.buttonContainer}
 							onPress = { 
 								() => {
-								//TODO: functionality here has a/some bug(s)
-								//if you never enter anything in a text field it will crash when you try to sign up
-								if(this.state.password != this.state.passwordConfirm)
-									Alert.alert('Sorry theres been a problem :(', 'Looks like your passwords do not match! Try entering them again.');
-								else if(this.state.email != this.state.emailConfirm) 
-									Alert.alert('Sorry theres been a problem :(', 'Looks like your emails do not match! Try entering them again.'); 
-								else
-								{ 
-									Alert.alert('Account Created', 'An account has been created under the name ' + this.state.username + '.  Happy Trading! :)'); 
+									Alert.alert('Account Created', 'An account has been created under the email ' + this.state.email + '.  Happy Trading! :)'); 
 									navigate('Login');
 								}
-							}
-						}  
+							}  
 						>
 						
 						<Text style={styles.buttonText}>SIGN UP</Text> 
