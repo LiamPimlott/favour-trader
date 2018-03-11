d1dcbb87720dd2c9f73f9abf25e5c22a291ca177
ismail khowaja <ismailkhowaja1@gmail.com>
2018-03-11 15:11:58 -0400

    Logout functionality

--- favour-trader-native/src/components/AppNavigation.js
+++ favour-trader-native/src/components/AppNavigation.js
@@ -3,14 +3,14 @@ import { Button } from 'react-native';
 import HomeScreen from '../views/HomeScreen.js';
 import Login from '../views/Login.js';
 import { StackNavigator, DrawerNavigator } from 'react-navigation';
-
-// Add conditional logic based on AuthService (When AuthService is merged in)
+import AuthService from "../components/AuthService";   
 
 const DrawerScreens = DrawerNavigator({
     Login: {
         screen: Login,
         navigationOptions: {
             header: null, // Login page should not show the navigation
+            drawerLabel: () => null,
         },
     },
     Home: {
@@ -25,9 +25,17 @@ export default AppNavigation = new StackNavigator({
     StackScreens: {
         screen: DrawerScreens,
         navigationOptions: ({ navigation }) => {
+            const doLogout =  () => {
+                let authService = new AuthService();
+                if (authService.loggedIn()) {
+                    authService.logout();
+                    navigation.navigate('Login');
+                }
+            };
             return ({
-                headerLeft: <Button color= {'rgba(0, 0, 0, 0)'} onPress={() => navigation.navigate('DrawerToggle')} title={'☰'} />,
-            });
+                headerLeft: <Button color={'rgba(0, 0, 0, 0)'} onPress={() => navigation.navigate('DrawerToggle')} title={'☰'} />,
+                headerRight: <Button color={'grey'} onPress={doLogout} title={'Log out'} />,
+            }); 
         },
     },
 });
