68ddb02af429af702c7291a10403bc99dc50c9a6
kerosenecity <alex.mike.mark@gmail.com>
2018-03-19 14:51:02 -0500

    alex-api-tests Wrote that skill test B)

--- favour-trader/test/skillsAPITest.js
+++ favour-trader/test/skillsAPITest.js
@@ -15,6 +15,36 @@ let should = chai.should();
 
 chai.use(chaiHttp);
 
+var skills = [
+    {
+        skill: "Fencing"
+    },
+    {
+        skill: "Dancing"
+    },
+    {
+        skill: "Programming"
+    },
+    {
+        skill: "Mathematics"
+    },
+    {
+        skill: "Plumbing"
+    },
+    {
+        skill: "Taxidermy"
+    },
+    {
+        skill: "Nuclear Physics"
+    },
+    {
+        skill: "Genetic Engineering"
+    },
+    {
+        skill: "Painting"
+    }
+]
+
 describe("Skill API Tests", () => {
     before((done) => {
         require('dotenv').config({ path: __dirname + '/../.env' });
@@ -38,5 +68,27 @@ describe("Skill API Tests", () => {
 
     describe("/ALL",()=>{
 
+        it("Should return a bunch of skills.",(done)=>{
+            Skill.insertMany(skills,(error,docs)=>{
+                chai.request(url)
+                .get("/api/skills/all")
+                .end((err,res)=>{
+                    expect(err).to.equal(null);
+                    expect(res.body).to.have.lengthOf(9);
+                    done();
+                })
+            })
+        })
+
+        
+        it("Should return nothing.",(done)=>{
+                chai.request(url)
+                .get("/api/skills/all")
+                .end((err,res)=>{
+                    expect(err).to.equal(null);
+                    expect(res.body).to.have.lengthOf(0);
+                    done();
+                })
+        })
     });
 })
