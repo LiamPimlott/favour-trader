0de4ae53be6a49d4a05aacd0c26b61df9834ab12
ismail khowaja <ismailkhowaja1@gmail.com>
2018-03-26 01:43:04 -0400

    Initial E2E tests for the Mobile.

--- favour-trader-native/acceptance/LoginTest_test.js
+++ favour-trader-native/acceptance/LoginTest_test.js
@@ -1,9 +1,18 @@
 
 Feature('LoginTest');
 
-Scenario('test something', (I) => {
+Scenario('User should be able to login with good credentials', (I, loginPage) => {
     I.wait(10);
-    I.fillField('~Email-Address', 'example@gmail.com');
-    I.fillField('~Password', 'password' );
-    I.tap('~Login')
+    loginPage.login('example@gmail.com', 'password');
+    I.tap('Select Filter');
+});
+
+Scenario('User should not be able to login with bad credentials @invalid', (I, loginPage) => {
+    I.wait(10);
+    loginPage.login('asd@gmail.com', 'password');
+    I.see('User not found');
+    I.tap('OK');
+    loginPage.login('example@gmail.com', 'passwor');
+    I.see('Incorrect password.');
+    I.tap('OK');
 });
