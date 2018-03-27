8a2c1c7a56636f1e7d8254eb367f354ebc18c279
Techman224 <jbernshine@gmail.com>
2018-03-26 23:23:02 -0500

    removed unused tests

--- favour-trader/client/src/test/e2e/userstories_test.js
+++ favour-trader/client/src/test/e2e/userstories_test.js
@@ -27,18 +27,3 @@ Scenario("Perfect Match Search @perfectMatch", (I, loginPage, profilePage, mainP
     I.click("View Profile");
     I.see("Mark Ripptoe");
 });
-
-/*
-Scenario("Offer a Trade @offerTrade", (I) => {
-    I.click("Offer a Trade!");
-    I.check("//input[@class='checkbox'][1]");
-    I.fillField("//textarea[@class='descriptionBox'][1]", "I would like to learn fencing");
-    I.click("Offers");
-    I.check("//td[contains(text(), 'Nuclear Physics')]/..//td[@class='checkboxTD']/input");
-    I.fillField("//td[contains(text(), 'Nuclear Physics')]/..//textarea[@class='descriptionBox']", "I can help you get plasma");
-    I.click("Message");
-    I.fillField('textarea#message.messageBox', "This is a good trade");
-    I.click('')
-
-});
-*/
